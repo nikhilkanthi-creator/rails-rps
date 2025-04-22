5b98907dc76d5ab2a95d316ca5432855a3cc5a86
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-22 22:29:39 +0000

    new pages made

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,14 +1,24 @@
 class ZebraController < ApplicationController
+  def monkey
+    render({ :template => "game_templates/homepage"})
+  end
+
   def giraffe
     @random_move = ["rock", "paper", "scissors"].sample
 
-    render({ :plain => "howdy"})
+    render({ :template => "game_templates/play_rock"})
   end
 
   def elephant
+
+    render({ :template => "game_templates/play_paper"})
+
   end
   
   def lion
+
+    render({ :template => "game_templates/play_scissors"})
+
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "zebra", :action => "monkey"})
+  
   get("/rock", { :controller => "zebra", :action => "giraffe"})
 
   get("/paper", { :controller => "zebra", :action => "elephant"})
