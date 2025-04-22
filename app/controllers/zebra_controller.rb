1720c50463289117199c72817696852976fde150
nikhilkanthi-creator <nkanthi@chicagobooth.edu>
2025-04-22 22:17:38 +0000

    scaffolding

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,5 +1,14 @@
 class ZebraController < ApplicationController
   def giraffe
+    @random_move = ["rock", "paper", "scissors"].sample
+
     render({ :plain => "howdy"})
   end
+
+  def elephant
+  end
+  
+  def lion
+  end
+  
 end
